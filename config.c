602a811bb2a9a206a80b3d12a297d3f66505b30b
xenoxanite <{user}@gmail.com>
2024-02-13 01:52:07 +0600

    Cpu script updated

--- config.c
+++ config.c
@@ -4,20 +4,12 @@
 #include "util.h"
 
 Block blocks[] = {
-  // {"bplayerctl",    0,  1 },
-  // {"bplayerctl-previous", 1,    2 },
-  // {"bplayerctl-play", 1,    3 },
-  // {"bplayerctl-next", 1,    4 },
-  // {"bupdate", 1800,    5 },
-  // {"bmic",   0,    4 },
-  // {"bvolume",   0,    6 },
-  // {"bbattery",    60, 8 },
-  // {"bnetwork",  10,   7 },
-  // {"$HOME/.suckless/dwmblocks/scripts/network", 5,  10},
+    {"$HOME/.suckless/dwmblocks/scripts/vpn", 5,  7},
+    {"$HOME/.suckless/dwmblocks/scripts/network", 5,  7},
     {"$HOME/.suckless/dwmblocks/scripts/volume", 1,  10},
     {"$HOME/.suckless/dwmblocks/scripts/mic",    1,  10},
-    {"$HOME/.suckless/dwmblocks/scripts/memory", 1,  10},
-    {"$HOME/.suckless/dwmblocks/scripts/cpu",    1,  10},
+    {"$HOME/.suckless/dwmblocks/scripts/memory", 3,  10},
+    {"$HOME/.suckless/dwmblocks/scripts/cpu",    3,  10},
     {"$HOME/.suckless/dwmblocks/scripts/date",   15, 10},
 };
 
