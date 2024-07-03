8367a0590ff9ecc54690e966c48d46fbdc5d0a8c
xenoxanite <xenoxanite@gmail.com>
2024-07-03 17:01:19 +0600

    Changed script executer

--- config.c
+++ config.c
@@ -4,7 +4,7 @@
 #include "util.h"
 
 Block blocks[] = {
-  {"$HOME/.suckless/dwmblocks/scripts/vpn",     5,  4},
+  // {"$HOME/.suckless/dwmblocks/scripts/vpn",     5,  4},
   // {"$HOME/.suckless/dwmblocks/scripts/network", 5,  3},
     {"$HOME/.suckless/dwmblocks/scripts/volume", 1,  2},
     {"$HOME/.suckless/dwmblocks/scripts/mic",    1,  1},
