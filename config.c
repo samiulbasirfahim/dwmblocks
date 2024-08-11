4f2523f711befa420ca908783467d533608a54a3
xenoxanite <xenoxanite@gmail.com>
2024-08-12 03:50:00 +0600

    Finish

--- config.c
+++ config.c
@@ -6,10 +6,10 @@
 Block blocks[] = {
   // {"$HOME/.suckless/dwmblocks/scripts/vpn",     5,  4},
   // {"$HOME/.suckless/dwmblocks/scripts/network", 5,  3},
-    {"$HOME/.suckless/dwmblocks/scripts/volume", 1,  2},
-    {"$HOME/.suckless/dwmblocks/scripts/mic",    1,  1},
-    {"$HOME/.suckless/dwmblocks/scripts/memory", 3,  0},
-    {"$HOME/.suckless/dwmblocks/scripts/cpu",    3,  0},
+    // {"$HOME/.suckless/dwmblocks/scripts/volume", 1,  2},
+    // {"$HOME/.suckless/dwmblocks/scripts/mic",    1,  1},
+    {"$HOME/.suckless/dwmblocks/scripts/volume_mic",    3,  0},
+    {"$HOME/.suckless/dwmblocks/scripts/memory_cpu",    3,  0},
     {"$HOME/.suckless/dwmblocks/scripts/date",   60, 0},
 };
 
