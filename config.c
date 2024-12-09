d42791448404824ba326d2fabc1970e83f9c625f
Samiul Basir Fahim <samiulbasirfahim.rxen@gmail.com>
2024-12-10 03:43:29 +0600

    updated

--- config.c
+++ config.c
@@ -4,13 +4,13 @@
 #include "util.h"
 
 Block blocks[] = {
-  // {"$HOME/.suckless/dwmblocks/scripts/vpn",     5,  4},
-  // {"$HOME/.suckless/dwmblocks/scripts/network", 5,  3},
-    // {"$HOME/.suckless/dwmblocks/scripts/volume", 1,  2},
-    // {"$HOME/.suckless/dwmblocks/scripts/mic",    1,  1},
-    {"$HOME/.suckless/dwmblocks/scripts/volume_mic",    2,  0},
-    {"$HOME/.suckless/dwmblocks/scripts/memory_cpu",    2,  0},
-    {"$HOME/.suckless/dwmblocks/scripts/date",   30, 0},
+    /* {"$HOME/.suckless/dwmblocks/scripts/vpn", 5, 4}, */
+    /* {"$HOME/.suckless/dwmblocks/scripts/network", 5, 3}, */
+    /* {"$HOME/.suckless/dwmblocks/scripts/volume", 1, 2}, */
+    {"$HOME/.suckless/dwmblocks/scripts/weather.sh",   60,  0},
+    /* {"$HOME/.suckless/dwmblocks/scripts/volume_mic",    3,  0}, */
+    {"$HOME/.suckless/dwmblocks/scripts/memory_cpu", 3, 0},
+    {"$HOME/.suckless/dwmblocks/scripts/date", 60, 0},
 };
 
 const unsigned short blockCount = LEN(blocks);
