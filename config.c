517f6a5a9d8387bc038b31e35425e4dc4490771d
Samiul Basir Fahim <xenoxanite@gmail.com>
2025-01-07 19:00:44 +0600

    up

--- config.c
+++ config.c
@@ -4,14 +4,10 @@
 #include "util.h"
 
 Block blocks[] = {
-    /* {"$HOME/.suckless/dwmblocks/scripts/vpn", 5, 4}, */
-    /* {"$HOME/.suckless/dwmblocks/scripts/network", 5, 3}, */
-    /* {"$HOME/.suckless/dwmblocks/scripts/volume", 1, 2}, */
-    {"$HOME/.suckless/dwmblocks/scripts/weather.sh",   60,  0},
-    /* {"$HOME/.suckless/dwmblocks/scripts/volume_mic",    3,  0}, */
-    {"$HOME/.suckless/dwmblocks/scripts/memory", 3, 0},
-    {"$HOME/.suckless/dwmblocks/scripts/cpu", 3, 0},
-    {"$HOME/.suckless/dwmblocks/scripts/date", 60, 0},
+    {"_dwmbar_weather",   60,  0},
+    {"_dwmbar_memory", 3, 0},
+    {"_dwmbar_cpu", 3, 0},
+    {"_dwmbar_date", 60, 0},
 };
 
 const unsigned short blockCount = LEN(blocks);
